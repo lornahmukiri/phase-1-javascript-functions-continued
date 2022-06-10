e2bfacccb649914af1622bacd2a58a7d1c316bf2
Lornah Mukiri <mukirikabuli@gmail.com>
2022-06-10 16:26:51 +0300

    commit tree b8e30440a912f3a9b5ba66f5e1e3dab5bb7daec0

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(task = `roller-skate`){
+    return `This Saturday, I want to ${task}!`
+}
+
+function mondayWork(task = `go to the office`){
+    return `This Monday, I will ${task}.`
+}
+
+
+function wrapAdjective (item = '*') {
+    return function (quality = `special`){
+        return `You are ${item}${quality}${item}!`
+    }
+  
+}
